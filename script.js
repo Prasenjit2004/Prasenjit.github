634c5b0c0a9131c5b08785ad7bf1690ecfee776a
Prasenjit2004 <dprasenjit3321@gmail.com>
2025-11-21 23:42:43 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-function showMessage() {
-    alert("Hello! You clicked the button. Welcome to JavaScript interactivity!");
-}
+let moon = document.querySelector('.moon');
+let button = document.getElementById('toggleBtn');
+let isAnimating = true;
+
+button.addEventListener('click', () => {
+    if (isAnimating) {
+        moon.style.animationPlayState = 'paused';
+        button.textContent = 'Start Animation';
+    } else {
+        moon.style.animationPlayState = 'running';
+        button.textContent = 'Stop Animation';
+    }
+    isAnimating = !isAnimating;
+});
